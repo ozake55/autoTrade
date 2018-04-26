1a52dd58fa4619dc79d4a1714876153118b25d64
ozakik <ozakik@10.40.59.10>
2018-04-26 19:54:59 +0900

    更新

--- src/test/autoTransaction/sbi/Login.java
+++ src/test/autoTransaction/sbi/Login.java
@@ -1,119 +1,81 @@
 package test.autoTransaction.sbi;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Login {
-
-	String ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36";
-
-	//String strURL = "https://site2.sbisec.co.jp/ETGate/?_ControlID=WPLETlgR001Control&_PageID=WPLETlgR001Rlgn50&_DataStoreID=DSWPLETlgR001Control&_ActionID=login&getFlg=on";
-	static String strURL = "https://site2.sbisec.co.jp/ETGate/";
-	
 	public static void main(String[] args) throws IOException {
-		//header
-		
-		//おそらくautoaa
-		//Content-Length: 552
-		
-		////////
-		//Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-		//Accept-Encoding: gzip, deflate, br
-		//Accept-Language: ja,en-US;q=0.9,en;q=0.8
-		//Cache-Control: max-age=0
-		//Connection: keep-alive
-		//Content-Type: application/x-www-form-urlencoded
-		
-		//Cookie
-		
-		//host:リクエストのホスト名やポート番号を通知
-		//Host: site2.sbisec.co.jp
-		
-		//origin:CORS(Cross-Origin Resource Sharing)によるクロスドメイン通信の対象ドメインリクエスト
-		//Origin: https://site2.sbisec.co.jp
+        Connection conn = null;
+        Response res = null;
+        Document doc = null;
 
-		//Referer:参照元ページの URL
-		//Referer: https://site2.sbisec.co.jp/ETGate/?_ControlID=WPLETlgR001Control&_PageID=WPLETlgR001Rlgn50&_DataStoreID=DSWPLETlgR001Control&_ActionID=login&getFlg=on
-		
-		//User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36
-		
-		////
-		//以下リクエストに設定しない
-		//Do Not Trackヘッダ
-		//DNT: 1
-		//UUpgrade-Insecure-Requests:1	画像などのHTML内Linkがhttpの場合に、clientが自動でhttpsとして解釈するよFlg
-		//Upgrade-Insecure-Requests: 1
+        // login_formの取得
+        String strURL = "https://site2.sbisec.co.jp/ETGate/";
+        conn = Jsoup
+            .connect(strURL)
+            .header("Accept",
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+            .header("Accept-Encoding", "gzip, deflate, br")
+            .header("Accept-Language", "ja,en-US;q=0.9,en;q=0.8")
+            .header("Host", "site1.sbisec.co.jp")
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
+            .header("Connection", "keep-alive").ignoreContentType(true).ignoreHttpErrors(true);
+        
+    try {
+            res = conn.execute();
+            doc = res.parse();
+            //System.out.println(doc.html());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
-		
-		//ユーザーネーム又はパスワードが正しくありません。(WBLE015002)
-		/*
-		<div id="user_input">
-        <input type="text" name="user_id" size="15" maxlength="32" style="width:170px;" id="imedisable" onfocus="Monc(&quot;UI&quot;)">
-       </div> </td> 
-      <td class="mtext" align="left" valign="top"> 
-       <div id="password_input">
-        <input type="password" name="user_password" maxlength="32" size="15" style="width:170px;" onfocus="Monc(&quot;PW&quot;)">
-       </div>
-       */
-		//<input type="radio" name="_ActionID" value="loginAcInfo">口座管理<br>
-		Document document = Jsoup.connect(strURL)
-				//Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-				.header("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-				//Accept-Encoding: gzip, deflate, br
-				.header("Accept-Encoding","gzip, deflate, br")
-				//Accept-Language: ja,en-US;q=0.9,en;q=0.8
-				.header("Accept-Language","ja,en-US;q=0.9,en;q=0.8")
-				//Cache-Control: max-age=0
-				.header("Cache-Control","max-age=0")
-				//Connection: keep-alive
-				.header("Connection","keep-alive")
-				//Content-Type: application/x-www-form-urlencoded
-				.header("Content-Type","application/x-www-form-urlencoded")
-				//host:リクエストのホスト名やポート番号を通知
-				//Host: site2.sbisec.co.jp
-				.header("Host","site2.sbisec.co.jp")
-				//origin:CORS(Cross-Origin Resource Sharing)によるクロスドメイン通信の対象ドメインリクエスト
-				//Origin: https://site2.sbisec.co.jp
-				.header("Origin","https://site2.sbisec.co.jp")
-				//Referer:参照元ページの URL
-				//Referer: https://site2.sbisec.co.jp/ETGate/?_ControlID=WPLETlgR001Control&_PageID=WPLETlgR001Rlgn50&_DataStoreID=DSWPLETlgR001Control&_ActionID=login&getFlg=on
-				.header("Referer","https://site2.sbisec.co.jp/ETGate/?_ControlID=WPLETlgR001Control&_PageID=WPLETlgR001Rlgn50&_DataStoreID=DSWPLETlgR001Control&_ActionID=login&getFlg=on")
-				//User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36
-				.header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36")
-				
-				////
-				.data("_PageID", "WPLETlgR001Rlgn10")
-				//.data("_DataStoreID", "xxxxxxxxxx_default_task_240_WPLETlgR001Rlgn50_login")
-				.data("_ControlID", "WPLETlgR001Control")
-				.data("_WID", "NoWID")
-				//.data("_ORGWID", "")
-				.data("_WID", "NoWID")
-				//.data("_WIDManager", "")
-				//.data("_WBSessionID", "xxxx_xxxx_xxxx_xxxxx")
-				//.data("_preProcess", "")
-				//.data("_TimeOutControl", "")
-				.data("_WIDMode", "0")
-				//.data("_WindowName", "")
-				//.data("_ReturnPageInfo", "")
-				.data("getFlg", "on")
-				.data("JS_FLG", "1")
-				.data("BW_FLG", "chrome,66")
-				.data("burl", "iris_top")
-				.data("cat1", "market")
-				.data("cat2", "top")
-				.data("dir", "tl1-top|tl2-map|tl5-jpn")
-				.data("file", "index.html")
-				.data("sw_page", "Banking")
-				//.data("sw_param1", "")
-				//.data("sw_param2", "")
-				.data("user_id", "yamada")
-				.data("user_password", "tarou")
-				.data("logon.x", "70")
-				.data("logon.y", "15")
-				.data("_ActionID", "loginAcInfo")
-				.post();
-		System.out.println(document.html());
+
+    //レスポンスから必要情報の抽出            
+        Elements form_login = doc.getElementsByAttributeValue("name","form_login");
+        Elements inputs = form_login.get(0).getElementsByTag("input");
+
+    //ログインに必要なパラメータの設定
+        HashMap<String, String> param = new HashMap<String, String>();
+        for(Element ele :inputs){
+            param.put(ele.attr("name"), ele.attr("value"));
+        }
+        param.put("user_id", "yamada");
+        param.put("user_password", "tarou");
+        
+        param.remove("ACT_login");
+        param.put("ACT_login.x", "94");
+        param.put("ACT_login.y", "16");
+        
+        param.put("BW_FLG", "chrome,65");
+        param.put("JS_FLG", "1");
+        param.put("_ActionID", "login");
+
+        //ログイン用アクセス
+        try {
+            //res = conn.data(param).cookies(res.cookies()).execute();
+            //doc = res.parse();
+            doc = conn.data(param).cookies(res.cookies()).post();
+            System.out.println(doc.html());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        //判定
+        if (res.cookies().containsKey("trading_site")) {
+        	System.out.println("success");
+            return;
+        } else {
+        	System.out.println("false");
+            return;
+        }
 	}
 }
