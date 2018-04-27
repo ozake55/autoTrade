2ee7324836ee3a297bf6dc8a693737f508da1535
ozaki <ozaki@192.168.1.9>
2018-04-27 17:31:37 +0900

    a

--- src/test/autoTransaction/sbi/Login.java
+++ src/test/autoTransaction/sbi/Login.java
@@ -30,7 +30,6 @@ public class Login {
 		                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
 		            .header("Accept-Encoding", "gzip, deflate, br")
 		            .header("Accept-Language", "ja,en-US;q=0.9,en;q=0.8")
-		            //.header("Host", "site1.sbisec.co.jp")
 		            .header("Host", host)
 		            .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
 		            .header("Connection", "keep-alive").ignoreContentType(true).ignoreHttpErrors(true);
@@ -40,14 +39,9 @@ public class Login {
         Response res = null;
         Document doc = null;
 
-        String strURL = "https://site1.sbisec.co.jp";
-        Connection conn = getConnect(strURL+"/ETGate/");
-        
-        // login_formの取得
-   
-        
+        String strURL = "https://site1.sbisec.co.jp"+"/ETGate/";
+        Connection conn = getConnect(strURL);
     try {
-    		//res = conn.execute();
             res = conn.method(Method.GET).execute();
             doc = res.parse();
             //doc = conn.get();
@@ -57,11 +51,9 @@ public class Login {
             return;
         }
 
-
-    //レスポンスから必要情報の抽出
+    //ログイン画面のinput tag を取得
         Elements form_login = doc.getElementsByAttributeValue("name","form_login");
         Elements inputs = form_login.get(0).getElementsByTag("input");
-        String form_login_action = form_login.attr("action");
 
         if (form_login.isEmpty()) {
         	System.out.println("form_login = null");
@@ -69,11 +61,13 @@ public class Login {
         }
 
         ////////
-    //ログインに必要なパラメータの設定
+        //パラメータの追加設定
         HashMap<String, String> param = new HashMap<String, String>();
         for(Element ele :inputs){
+        	//inpu tagを再設定
             param.put(ele.attr("name"), ele.attr("value"));
         }
+        //追加
         param.put("user_id", userid);
         param.put("user_password", password);
         
@@ -85,8 +79,7 @@ public class Login {
         param.put("JS_FLG", "1");
         param.put("_ActionID", "login");
 
-        conn = getConnect(strURL+form_login_action);
-        //ログイン用アクセス
+        conn = getConnect(strURL);
         try {
             res = conn.data(param).cookies(res.cookies()).method(Method.POST).execute();
         } catch (IOException e) {
@@ -95,21 +88,23 @@ public class Login {
         }
         doc = res.parse();
         //doc = conn.data(param).cookies(res.cookies()).post();
-        System.out.println(doc.html());
+        //System.out.println(doc.html());
         ////////
         
         ////////
-        //formSwitch check        
+        //formSwitch check
         Elements formSwitch = doc.getElementsByAttributeValue("name","formSwitch");
         if (!formSwitch.isEmpty()) {
-        	String formSwitch_action = formSwitch.attr("action");
+        	//String formSwitch_action = formSwitch.attr("action");
+        	strURL = formSwitch.attr("action");
+        	
         	Elements formSwitch_inputs = formSwitch.get(0).getElementsByTag("input");
             //ログインに必要なパラメータの設定
             HashMap<String, String> formSwitch_param = new HashMap<String, String>();
             for(Element ele :formSwitch_inputs){
             	formSwitch_param.put(ele.attr("name"), ele.attr("value"));
             }
-            conn = getConnect(formSwitch_action);
+            conn = getConnect(strURL);
             try {
                 res = conn.data(formSwitch_param).cookies(res.cookies()).method(Method.POST).execute();
             } catch (IOException e) {
@@ -117,17 +112,26 @@ public class Login {
                 return;
             }
             doc = res.parse();
-            //doc = conn.data(param).cookies(res.cookies()).post();
-            System.out.println(doc.html());
             ////////
         }
+        //ログイン後画面
+        //System.out.println(doc.html());      
+
+        ////////
+        //口座管理画面遷移
+        String AccountStrUrl =strURL+"?_ControlID=WPLETacR001Control&_PageID=DefaultPID&_DataStoreID=DSWPLETacR001Control&_ActionID=DefaultAID&getFlg=on";
+        //レスポンスから必要情報の抽出
+        conn = getConnect(AccountStrUrl);
+        try {
+            res = conn.cookies(res.cookies()).method(Method.GET).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        doc = res.parse();
+        System.out.println(doc.html());
         
-
-        //https://www.sbisec.co.jp/ETGate/?OutSide=on&_ControlID=WPLETacR001Control&_PageID=DefaultPID&_DataStoreID=DSWPLETacR001Control&_SeqNo=2003_06_12_10_02_34.574_ExecuteThread%3A__45__for_queue%3A__wplExecute_Queue__WPLETlgR001Rlgn20_login&getFlg=on&_ActionID=DefaultAID&int_pr1=150313_cmn_gnavi:2_dmenu_01
-        //レスポンスから必要情報の抽出            
-        Elements link2_account_menu = doc.getElementsByAttributeValue("id","link2_account_menu");
-        //Elements tag_a = link2_account_menu.get(0).getElementsByTag("a");
-
+        
         //判定
         if (res.cookies().containsKey("trading_site")) {
         	System.out.println("success");
