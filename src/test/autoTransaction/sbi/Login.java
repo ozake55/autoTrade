bfd8f30a9ece3ec151c64f582189095775bd8cbf
ozaki <ozaki@192.168.0.24>
2018-05-02 16:08:16 +0900

    a

--- src/test/autoTransaction/sbi/Login.java
+++ src/test/autoTransaction/sbi/Login.java
@@ -3,7 +3,9 @@ package test.autoTransaction.sbi;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.jsoup.Connection;
 import org.jsoup.Connection.Method;
@@ -11,6 +13,7 @@ import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class Login {
@@ -18,42 +21,57 @@ public class Login {
 	static String userid = System.getenv("userid");
 	static String password = System.getenv("password");
 
-	// static String baseURL = "https://site1.sbisec.co.jp"+"/ETGate/";
 	static Response res = null;
 	static Document doc = null;
 
 	HostManage host = null;
-	
-	//String query = null;
 	
 	Login(HostManage _host){
 		host = _host;
 		//query = _query;
 	}
 	
-	/*
-	Response logout(String _query) throws IOException {
+	boolean logout(String _query) throws IOException {
 		
-	}
-	*/
-	
-	Response execute(String _query) throws IOException {
-		
-		Connection conn = host.getConnect(_query);
-		
-			res = conn.method(Method.GET).execute();
+		if (res == null) {
+			return false;
+		}
 
-			/*cookie check
-			// 判定
-			if (res.cookies().containsKey("trading_site")) {
-				System.out.println("success");
-			} else {
-				System.out.println("false");
+		Connection conn = host.getConnect(_query);
+		// res = conn.cookies(res.cookies()).method(Method.GET).execute();
+		// formSwitchはログイン時だけだと思うけど。。
+		res = host.formSwitch(conn.cookies(res.cookies()).method(Method.GET).execute());
+
+		doc = res.parse();
+		// System.out.println(doc.html());
+
+		Elements div = doc.getElementsByAttributeValue("class", "alC");
+
+		ArrayList<String> list = new ArrayList<>();
+		for (Element ele : div) {
+			List<Node> nodes = ele.childNodes();
+			for (Node node : nodes) {
+				if (node instanceof Element) {
+					Elements p = ((Element) node).getElementsByTag("p");
+					for (Element element : p) {
+						if (element.attr("class").equals("fl01")) {
+							// "SBI証券をご利用いただきありがとうございました。"
+							System.out.println(element.childNode(0).toString().trim());
+							return true;
+						}
+					}
+				}
 			}
-			*/
-			
-			doc = res.parse();
-			// System.out.println(doc.html());
+		}
+		return false;
+	}
+	
+	Response login(String _query) throws IOException {
+
+		Connection conn = host.getConnect(_query);
+
+		res = conn.method(Method.GET).execute();
+		doc = res.parse();
 
 		// ログイン画面のinput tag を取得。ログイン後の画面によってform名が異なるので注意！
 		Elements form_login = doc.getElementsByAttributeValue("name", "MyForm01");
@@ -61,49 +79,50 @@ public class Login {
 			System.out.println("form_login = null");
 			return null;
 		}
-		
-		Elements inputs = form_login.get(0).getElementsByTag("input");
 
+		Elements inputs = form_login.get(0).getElementsByTag("input");
 
 		host.getParam(inputs);
 		////////
-		//ログイン
+		// ログイン
 		// formパラメータの設定
 		HashMap<String, String> param;
 		param = host.getParam(inputs);
 		param.put("user_id", userid);
 		param.put("user_password", password);
+		//param.put("user_id", "yamada");
+		//param.put("user_password", "tarou1234");
 
 		conn = host.getConnect("");
 
-		//formSwitch＋ログイン
+		// formSwitch＋ログイン
 		res = host.formSwitch(conn.data(param).cookies(res.cookies()).method(Method.POST).execute());
 		// ログイン後の画面
-		//doc = res.parse();
-		//System.out.println(doc.html());
+		// doc = res.parse();
+		// System.out.println(doc.html());
 		/////
-		
-		////
-		//ログインできているか、response headerのcookieをチェック。
-		//以後のresponseには新たなset-cookieは含まれないので注意
-				if (res.cookies().containsKey("trading_site")) {
-					System.out.println("success");
-				} else {
-					System.out.println("false");
-				}
-		
-		////////
-		//ログイン後の画面は口座画面かチェック（お知らせ等表示される場合がある）
 
-		//もしも、口座画面で無い場合、再度ログイン画面に遷移
+		////
+		// ログインできているか、response headerのcookieをチェック。
+		// 以後のresponseには新たなset-cookieは含まれないので注意
+		if (res.cookies().containsKey("trading_site")) {
+			System.out.println("login success");
+		} else {
+			System.out.println("login false");
+			return null;
+		}
+
+		////////
+		// ログイン後の画面は口座画面かチェック（お知らせ等表示される場合がある）
+
+		// もしも、口座画面で無い場合、再度ログイン画面に遷移
 		conn = host.getConnect(_query);
-		//res = conn.cookies(res.cookies()).method(Method.GET).execute();
-		//formSwitchはログイン時だけだと思う。。
+		// res = conn.cookies(res.cookies()).method(Method.GET).execute();
+		// formSwitchはログイン時だけだと思う。。
 		res = host.formSwitch(conn.cookies(res.cookies()).method(Method.GET).execute());
-		////////		
+		////////
 		return res;
 	}
-	
 	
 	public static void main(String[] args) throws IOException {
 
@@ -115,13 +134,17 @@ public class Login {
 		String startQuery = "_ControlID=WPLETacR001Control&_DataStoreID=DSWPLETacR001Control&_PageID=DefaultPID&_ActionID=DefaultAID&getFlg=on";
 		//ホーム
 		//String startQuery =  "_ControlID=WPLEThmR001Control&_DataStoreID=DSWPLEThmR001Control&_PageID=DefaultPID&_ActionID=DefaultAID&getFlg=on";
-		//String logoutQuery = "_ControlID=WPLETlgR001Control&_DataStoreID=DSWPLETlgR001Control&_PageID=WPLETlgR001Rlgn50&_ActionID=logout&getFlg=on";
-
-		res = login.execute(startQuery);
-
-		// ログイン後の画面
-		doc = res.parse();
-		System.out.println(doc.html());
+		String logoutQuery = "_ControlID=WPLETlgR001Control&_DataStoreID=DSWPLETlgR001Control&_PageID=WPLETlgR001Rlgn50&_ActionID=logout&getFlg=on";
+		
+		res = login.login(startQuery);
+		if (res != null){
+			// ログイン後の画面
+			doc = res.parse();
+			System.out.println(doc.html());
+			
+			//LOGOUT
+			login.logout(logoutQuery);
+		}
 		
 
 	}
