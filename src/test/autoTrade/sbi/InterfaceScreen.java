93d2cbd0c4a7fd9a98f702341155cfd7ec7d709b
ozaki <ozaki@192.168.1.7>
2018-05-09 04:07:14 +0900

    a

---
+++ src/test/autoTrade/sbi/InterfaceScreen.java
@@ -0,0 +1,16 @@
+package test.autoTrade.sbi;
+
+import java.io.IOException;
+
+import org.jsoup.Connection.Response;
+import org.jsoup.nodes.Document;
+
+public interface InterfaceScreen {
+	Login getLogin();
+	void setLogin(Login login);
+	//Response getRes();
+	//void setRes(Response res);
+	//SbiUtil getSbiUtil();
+	//void setSbiUtil(SbiUtil sbiUtil);
+	Document getScreen() throws IOException;
+}
